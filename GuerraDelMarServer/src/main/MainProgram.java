7d42776d6e5668be481c88672e139178c7a11fb0
nlrarojas <nlrarojas@hotmail.com>
2017-06-10 22:56:01 -0600

    Estructura básica del proyecto

---
+++ GuerraDelMarServer/src/main/MainProgram.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ *
+ * @author Nelson
+ */
+public class MainProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
